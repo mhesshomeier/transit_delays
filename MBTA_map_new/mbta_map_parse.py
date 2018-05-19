07c2bf427187bf44712eded871373d3f2de3d30a
mhesshomeier <mjhh@mit.edu>
2018-05-19 11:09:25 -0400

    mbta parse edit

--- MBTA_map_new/mbta_map_parse.py
+++ MBTA_map_new/mbta_map_parse.py
@@ -10,7 +10,7 @@ MBTA_api = "wX9NwuHnZU2ToO7GmGR9uw"
 
 # make sure I'm in the current directory
 import os
-os.chdir('/Users/abob/Desktop/github/big-data-spring2018/transit_delays/MBTA_map_new')
+os.chdir('/Users/meganhess-homeier/github/transit_delays/')
 
 
 #function for parsing json network file to create source target dataframe
@@ -47,3 +47,52 @@ for i in data["links"]:
 sourc_targ_fin = pd.concat([sourc_targ_df, sourc_targ_df2], axis = 1)
 
 data["parentStops"].str.contains(data["nodes"][data["links"][0]["source"]]["id"])
+
+
+
+print(sourc_targ_fin)
+
+
+df_source_target = sourc_targ_fin.rename({'0': 'source', '0': 'target'}, axis = 'columns')
+
+sourc_targ_fin.columns
+# change column names to reflect the origin and destination stops
+df_source_target.columns = ['to_stop', 'from_stop']
+
+print(df_source_target)
+
+# create csv with the origin and destination stops but nothing else
+df_source_target.to_csv('source_target.csv')
+
+print (df_source_target)
+
+# read in the file with all the stop info including old id, new id, station name, etc
+df_key = pd.read_csv('scripts/data/stops_all.csv')
+
+df_key.head()
+# make a smaller dataframe with just the data we need
+key_new = df_key[['stop_name_new', 'stop_id_new', 'stop_id_old']].copy()
+
+key_new.head()
+
+# merge the data for the station name and stop id for the destination stop
+df_stoppairs = pd.merge(left=df_source_target,right=key_new, left_on='to_stop', right_on='stop_id_new')
+# tried to do an inner join to drop additional rows (need to only keep the rows that have values in the
+# to_stop' and 'from_stop fields')
+
+# this is too long- must have rows wiht NAN vlaues for to_stop and from_stop
+df_stoppairs.shape
+df_source_target.shape
+print(df_stoppairs)
+
+
+df_stoppairs.tail()
+# dropna didn't seem to work
+df_stop_pair2 = df_stoppairs.dropna(axis=0, how='any', subset=['to_stop', 'from_stop'], inplace=False)
+
+df_stop_pair2.shape
+# drop duplicates works but only have 114 rows, used to have 116
+df_stop_pair2.drop_duplicates(subset=['to_stop', 'from_stop'], inplace = True)
+df_stop_pair2.shape
+
+# df_stop_pair2.to_csv('MBTA_map_new/stop_list.csv')
